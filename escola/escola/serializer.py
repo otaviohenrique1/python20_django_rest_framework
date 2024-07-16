dc5b58d04486451fdedd87274ed3d181cf1b9f82
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-07-15 21:21:57 -0300

    commit 1

---
+++ escola/escola/serializer.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from escola.models import Estudante, Curso, Matricula
+
+
+class EstudanteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estudante
+        fields = ["id", "nome", "email", "cpf", "data_nascimento", "celular"]
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = "__all__ "
+
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
+
+
+class ListaMatriculasEstudantesSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source="curso.descricao")
+    periodo = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Matricula
+        fields = ["curso", "periodo"]
+
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+
+
+class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source="estudante.nome")
+    
+    class Meta:
+        model = Matricula
+        fields = ["estudante_nome"]
